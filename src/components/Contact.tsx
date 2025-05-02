d6b491497f54501d5a2d81ce58f1f6d6d6a1c53f
MudipaK <mudipa.i@axceera.com>
2025-05-02 23:49:07 +0530

    up

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -76,17 +76,7 @@ const Contact = () => {
               </div>
             </div>
 
-            <div className="mt-8 h-64 rounded-lg overflow-hidden">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.123456789012!2d80.12345678901234!3d6.123456789012345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMDcnMjQuNCJOIDgwwrAwNycxMi4wIkU!5e0!3m2!1sen!2slk!4v1234567890123!5m2!1sen!2slk"
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
-                allowFullScreen
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              />
-            </div>
+         
           </motion.div>
 
           <motion.div
